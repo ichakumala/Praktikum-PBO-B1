be7d5f6d09486d507653d2d9775c8f20803f27cd
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-03-15 16:42:24 +0700

    Update TimSepakbola.java

--- POSTEST/TimSepakbola.java
+++ POSTEST/TimSepakbola.java
@@ -1,3 +1,10 @@
+/* Nama File	: TimSepakbola.java
+   Nama		    : Annisa Kumala Dewi
+   NIM		    : 24060121120025
+   Tanggal	  : 15 Maret 2023
+   Deskipsi	  : Kelas yang berisi program utama yang berisi kelas TimSepakbola 
+   Lab		    : B1*/
+
 class TimSepakbola {
 
   // Assume there are only 2 teams using this class!

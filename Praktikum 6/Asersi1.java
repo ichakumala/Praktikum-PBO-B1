d2845e0bd2b9811e5d4707650dba2dcc939807c0
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-04-03 21:22:02 +0700

    Update Asersi1.java

--- Praktikum 6/Asersi1.java
+++ Praktikum 6/Asersi1.java
@@ -1,3 +1,10 @@
+/*
+Nama File   : Asersi1.Java 29/03/23
+Pembuat     : Annisa Kumala Dewi - 24060121120025
+Deskripsi   : Program untuk menentukan asersi
+Lab         : B1
+*/
+
 public class Asersi1{
     public static void main (String[] agrs){
         int x = 0;

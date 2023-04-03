f500adab7b9ae87fafce0c071f1352f1336492bc
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-04-03 21:19:10 +0700

    Update AngkaSial.java

--- Praktikum 6/AngkaSial.java
+++ Praktikum 6/AngkaSial.java
@@ -1,3 +1,12 @@
+/*
+Nama File   : AngkaSial.Java 29/03/23
+Pembuat     : Annisa Kumala Dewi - 24060121120025
+Deskripsi   : Program penggunaan exception buatan sendiri
+              Pengenalan klausa 'throw' dan 'throws'
+Lab         : B1
+*/
+
+
 public class AngkaSial{
 
     public void cobaAngka(int angka) throws AngkaSialException{
@@ -13,10 +22,16 @@ public class AngkaSial{
             as.cobaAngka(16);
             as.cobaAngka(13);
             as.cobaAngka(12);
-        }
-        catch(AngkaSialException ase){
+        }catch(AngkaSialException ase){
+            
             System.out.println(ase.getMessage());
             System.out.println("hati - hati memasukkan angka!!!");
         }
     }
 }
+
+/*
+Pertanyaan  : Ketika eksepsi terjadi, apakah baris 12 pada AngkaSial.java di atas dieksekusi? Apakah baris 21 pada AngkaSial.java di atas dieksekusi?
+Jawab       : Baris ke 12 dan ke 21 pada AngkaSial tidak dapat dieksekusi karena adanya kebutuhan untuk membuat class exception
+              Angka sial terlebih dahulu yang akan menjadi turunan dari class exception. Oleh karena itu perlu dibuat class exception dahulu sebelum menggerjakan baris tersebut. 
+*/

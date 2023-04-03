07eb133d7215c0e72d7a181196f6f7163835041f
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-04-03 21:20:28 +0700

    Update AngkaSialException.java

--- Praktikum 6/AngkaSialException.java
+++ Praktikum 6/AngkaSialException.java
@@ -1,3 +1,10 @@
+/*
+Nama File   : AngkaSialExceptiom.Java 29/03/23
+Pembuat     : Annisa Kumala Dewi - 24060121120025
+Deskripsi   : Eksepsi buatan sendiri, menolak masukan angka 13!
+Lab         : B1
+*/
+
 public class AngkaSialException extends Exception{
     public AngkaSialException(){
         super("Jangan masukkan angka 13 karena angka sial !!!");

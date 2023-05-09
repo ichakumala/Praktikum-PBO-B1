55297ddca933f6bf95f2935435e789cc96b333fe
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-09 20:55:35 +0700

    Update Sewa.java

--- Praktikum 7/PI Vechile/Sewa.java
+++ Praktikum 7/PI Vechile/Sewa.java
@@ -1,3 +1,10 @@
+/*
+File    : Sewa.java
+Pembuat : Annisa Kumala Dewi/24060121120025
+Tanggal : 3 Mei 2023
+Lab     : B1
+ */
+
 public class Sewa{
     public static void main(String[] args){
         Vehicle kendaraan = new Vehicle();

d4cc5d8bbb8404d56258cbf70ceaceffd8cd6e04
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-09 20:56:16 +0700

    Update Vehicle.java

--- Praktikum 7/PI Vechile/Vehicle.java
+++ Praktikum 7/PI Vechile/Vehicle.java
@@ -1,3 +1,10 @@
+/*
+File    : Vehicle.java
+Pembuat : Annisa Kumala Dewi/24060121120025
+Tanggal : 3 Mei 2023
+Lab     : B1
+ */
+
 public class Vehicle{
     void calRent(int distance, float price){
         float fare=distance*price;

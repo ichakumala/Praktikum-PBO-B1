1c26017125fbfb46da379367e596c6cc152bdcfc
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-09 20:55:04 +0700

    Update Car.java

--- Praktikum 7/PI Vechile/Car.java
+++ Praktikum 7/PI Vechile/Car.java
@@ -1,3 +1,10 @@
+/*
+File    : Car.java
+Pembuat : Annisa Kumala Dewi/24060121120025
+Tanggal : 3 Mei 2023
+Lab     : B1
+ */
+
 public class Car extends Vehicle{
     void calRent(int jarak, float harga){
         float fare=jarak*harga;

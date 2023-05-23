7eef0d2795bd3b3b79c4953725ca8cfd67bc93a1
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-23 20:25:30 +0700

    Update BangunDatarGeneric.java

--- Praktikum 8/BangunDatarGeneric.java
+++ Praktikum 8/BangunDatarGeneric.java
@@ -1,10 +1,17 @@
-public class BangunDatarGeneric<T extends BangunDatar>{
-    private T bangunDatar;
+/*
+NamaFile    : BangunDatarGeneric.java
+Nama        : Annisa Kumala Dewi
+NIM         : 24060121120025
+Deskripsi   : Kelas konstruksi generic bangun datar
+*/
+
+public class BangunDatarGeneric<T1 extends BangunDatar>{
+    private T1 bangunDatar;
     public void set(T tipeBangunDatar){
         bangunDatar = tipeBangunDatar;
     }
 
-    public T get(){
+    public T1 get(){
         return bangunDatar;
     }
 
@@ -12,6 +19,15 @@ public class BangunDatarGeneric<T extends BangunDatar>{
         return bangunDatar.hitungKeliling();
     }
 }
+
+/*
+Jika "T" diganti mengganti karakter lain seperti "T1", maka segala kemunculan
+"T" menjadi T1. Karena hal tersebut akan mempengaruhi deklarasi kelas, method, variabel, 
+dan parameter yang menggunakan parameter tipe generic. Selain itu semua penggunaan "T"
+dalam parameter metode, variabel lokal, dan pemanggilan metode juga harus diganti dengan 
+karakter baru seperti "T1" atau lainnya sesuai dengan perubahan yang dilakukan.
+Penggantian karakter perlu dilakukan secara konsisten agar tidak ada kesalahan program.
+*/
 
 
 

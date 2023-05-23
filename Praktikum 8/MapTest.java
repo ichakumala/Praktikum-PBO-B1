9d92c62aa6b02cc9143d4749530d9cfe6e544369
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-23 20:29:12 +0700

    Update MapTest.java

--- Praktikum 8/MapTest.java
+++ Praktikum 8/MapTest.java
@@ -1,14 +1,24 @@
+/*
+NamaFile    : MapTest.java
+Nama        : Annisa Kumala Dewi
+NIM         : 24060121120025
+Deskripsi   : Program penggunaan objek Array List sebagai collection class
+*/
+
 import java.util.*;
 
 public class MapTest {
     public static void main(String[] args){
         //kunci -> integer, nilai -> string 
         Map<Integer, String> map = new HashMap<Integer, String>();
+        
         // menempatkan elemen kunci dan nilai
         map.put(1, "satu");
         map.put(2, "dua");
+        
         //mengambil elemen pertama
         System.out.println(map.get(1));
+        
         //mengambil keseluruhan kunci sebagai objek collection set
         Set<Integer> key = map.keySet();
     }

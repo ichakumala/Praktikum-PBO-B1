fbedb4d9ee342354885103b09cc78691a3820ed9
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-23 20:20:21 +0700

    Update BangunDatar.java

--- Praktikum 8/BangunDatar.java
+++ Praktikum 8/BangunDatar.java
@@ -1,3 +1,10 @@
+/*
+NamaFile    : BangunDatar.java
+Nama        : Annisa Kumala Dewi
+NIM         : 24060121120025
+Deskripsi   : Kelas abstrak untuk bangun datar
+*/
+
 public abstract class BangunDatar{
     public abstract double hitungKeliling();
 }

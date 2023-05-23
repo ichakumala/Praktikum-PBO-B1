88ea9a29b1a0654c4d0255387bcaaad61ff7c6b9
Annisa Kumala Dewi <100399671+ichakumala@users.noreply.github.com>
2023-05-23 20:19:31 +0700

    Update ArrayListTest.java

--- Praktikum 8/ArrayListTest.java
+++ Praktikum 8/ArrayListTest.java
@@ -1,16 +1,25 @@
+/*
+NamaFile    : ArrayList.java
+Nama        : Annisa Kumala Dewi
+NIM         : 24060121120025
+Deskripsi   : Program penggunaan objek Array List sebagai collection class
+*/
+
 import java.util.ArrayList;
 
 public class ArrayListTest{
     public static void main(String[] args){
         //inisialisasi ArrayList yang hanya dapat berisi objek String    
         ArrayList<String> strings = new ArrayList<String>();
-        // menambah elemen
         
+        // menambah elemem
         strings.add("praktikum");
         strings.add("collection");
         strings.add("dan generics");
+        
         //menghapus elemen
         strings.remove("praktikum");
+        
         //iterasi pada keseluruhan ArrayList
         for(String s : strings) {
             System.out.print(s+" ");
